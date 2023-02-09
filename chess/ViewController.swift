a5f9f292b3ed18b29b61f97ac8dd9e2d20edbfb9
alishatsai <aliciatsai919@gmail.com>
2023-02-10 07:40:35 +0800

    Initial Commit

---
+++ chess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chess
+//
+//  Created by Alisha on 2023/2/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
