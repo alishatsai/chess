58a86dcd28738d8e2041dccb6ca62984586e466b
alishatsai <aliciatsai919@gmail.com>
2023-02-11 22:58:17 +0800

    建立棋盤底層contentView，squareImageView

--- chess/ViewController.swift
+++ chess/ViewController.swift
@@ -11,7 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let count: CGFloat = 8.0
+        let squareW = self.view.frame.width/(count+1.0)
+        self.view.backgroundColor = UIColor.black
+        let contentView = UIView()
+        contentView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.width)
+        contentView.center = self.view.center
+        self.view.addSubview(contentView)
+        
+        let squareImageView:UIImageView = UIImageView()
+        squareImageView.contentMode = UIView.ContentMode.scaleAspectFit
+        squareImageView.frame = contentView.bounds
+        squareImageView.backgroundColor = UIColor.lightGray
+        contentView.addSubview(squareImageView)
+
     }
 
 
