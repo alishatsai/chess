47dc42221e846de333acc334bd478a7becbad76d
alishatsai <aliciatsai919@gmail.com>
2023-02-12 09:22:48 +0800

    Add New Color Set and Extension to UIColor

--- chess/ViewController.swift
+++ chess/ViewController.swift
@@ -12,20 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let count: CGFloat = 8.0
-        let squareW = self.view.frame.width/(count+1.0)
+        let count: Int = 8
+        let squareW = self.view.frame.width/CGFloat(count)
         self.view.backgroundColor = UIColor.black
         let contentView = UIView()
         contentView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.width)
         contentView.center = self.view.center
         self.view.addSubview(contentView)
         
-        let squareImageView:UIImageView = UIImageView()
-        squareImageView.contentMode = UIView.ContentMode.scaleAspectFit
-        squareImageView.frame = contentView.bounds
-        squareImageView.backgroundColor = UIColor.lightGray
-        contentView.addSubview(squareImageView)
-
     }
 
 
---
+++ chess/extension.swift
@@ -0,0 +1,14 @@
+//
+//  extension.swift
+//  chess
+//
+//  Created by Alisha on 2023/2/12.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let lightBrick: UIColor = #colorLiteral(red: 0.9176470588, green: 0.8431372549, blue: 0.7254901961, alpha: 1)
+    static let darkBrick: UIColor = #colorLiteral(red: 0.6745098039, green: 0.5411764706, blue: 0.4156862745, alpha: 1)
+}
