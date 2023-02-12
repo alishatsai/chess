5038f7d86e1c756618ecf6f504d3e49a5d28ed52
alishatsai <aliciatsai919@gmail.com>
2023-02-12 10:54:48 +0800

    Setup Chess Map Use nested loop to set up chess map

--- chess/ViewController.swift
+++ chess/ViewController.swift
@@ -13,12 +13,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let count: Int = 8
-        let squareW = self.view.frame.width/CGFloat(count)
+        let brickWidth = self.view.frame.width/CGFloat(count)
         self.view.backgroundColor = UIColor.black
         let contentView = UIView()
         contentView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.width)
         contentView.center = self.view.center
         self.view.addSubview(contentView)
+        
+        // chess map
+        for i in 0...count-1 {
+            for j in 0...count-1 {
+                let brickImageView:UIImageView = UIImageView()
+                brickImageView.contentMode = UIView.ContentMode.scaleAspectFit
+                brickImageView.frame = CGRect(x:CGFloat(i)*brickWidth , y: 0, width: brickWidth, height: brickWidth)
+                brickImageView.center.y = CGFloat(j)*brickWidth
+                if (i+j) % 2 == 0 {
+                    brickImageView.backgroundColor = .darkBrick
+                }else {
+                    brickImageView.backgroundColor = .lightBrick
+                }
+                contentView.addSubview(brickImageView)
+            }
+            
+        }
         
     }
 
